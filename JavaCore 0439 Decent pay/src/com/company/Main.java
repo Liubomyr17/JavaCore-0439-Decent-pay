8afc499738aebfefd976efe10d8cb5f2e83dc953
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-05 12:05:03 +0200

    Add files via upload

---
+++ JavaCore 0439 Decent pay/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/*
+
+0438 Рисуем линии
+Используя цикл for вывести на экран:
+— горизонтальную линию из 10 восьмёрок
+— вертикальную линию из 10 восьмёрок.
+Требования:
+1. Программа не должна считывать числа c клавиатуры.
+2. Программа должна выводить числа на экран.
+3. Программа должна выводить горизонтальную линию из 10 восьмёрок.
+4. Программа должна выводить вертикальную линию из 10 восьмёрок.
+5. В программе должен использоваться цикл for.
+
+*/
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+     for (int i = 0; i < 100; i++) {
+         System.out.println("I will never work for a penny. Amigo");
+     }
+    }
+}
+
+
